bb82368ef212a45b770735612b64ff4abea0993a
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2018-10-18 20:54:32 +0530

    Added angular routes and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Incident 2019' }
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
