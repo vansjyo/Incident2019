45c82f5bdc524d60d7e24346b77ace534522feaa
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2018-10-24 15:43:39 +0530

    Added social media links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 
+const SOCIAL = {
+  'fb': {
+    'link': 'facebook.com',
+    'image': 'assets/images/social/fb.svg'
+  },
+  'tw': {
+    'link': 'twitter.com',
+    'image': 'assets/images/social/tw.svg'
+  },
+  'yt': {
+    'link': 'youtube.com',
+    'image': 'assets/images/social/yt.svg'
+  },
+  'ig': {
+    'link': 'instagram.com',
+    'image': 'assets/images/social/ig.svg'
+  }
+};
+
 function toggleMenu() {
   const header = $('.header');
   const i = $('.header i');
@@ -20,6 +39,8 @@ function toggleMenu() {
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+
+  social = SOCIAL;
 
   isHome() {
     return window.location.pathname === '/';
