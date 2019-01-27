c3b805bd37189d3e3f887c3788208f13674def9a
vansjyo <vanshika421@gmail.com>
2019-01-27 21:31:39 +0530

    added sorting to timeline

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -21,12 +21,26 @@ export class TimelineComponent implements OnInit {
   timeline = TIMELINE;
   now = new Date();
   state = this.now.getDate();
+  
   display : any;
+
+
+
   getparsed(puttime){
     return new Date(puttime);
   }
   constructor(private titleService: Title) {
     this.titleService.setTitle('Incident 2019 - Timeline');
+    console.log(this.timeline);
+    this.timeline.sort(function(a, b){
+      var a1 = new Date(a.time_start);
+      var b1 = new Date(b.time_start);
+      return a1.getTime() - b1.getTime()
+    });
+    console.log(this.timeline);
+    if(this.state!=15 && this.state!=16 && this.state!=17){
+      this.state=15;
+    }
     this.display = function(stateinput){
       this.state = stateinput;
       return;
